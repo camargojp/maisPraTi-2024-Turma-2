c110ac0272c6375dc3c3556f363af37b6b720611
camargojp <jpcruzcamargo@gmail.com>
2025-02-03 21:43:57 -0300

    lesson 47

---
+++ 3-reactjs/5-conditionalRendering/src/App.jsx
@@ -0,0 +1,16 @@
+import './App.css'
+import Login from './components/login'
+import UserList from './components/UserList'
+import Counter from './components/Counter'
+import Timer from './components/Timer'
+
+function App() {
+
+  return (
+    <>
+      <Timer />
+    </>
+  )
+}
+
+export default App
