8cb78bdbb4aab71429354a0994f1547491233087
camargojp <jpcruzcamargo@gmail.com>
2025-02-04 21:55:20 -0300

    lesson 48

---
+++ 3-reactjs/6-context/src/App.jsx
@@ -0,0 +1,16 @@
+import './App.css'
+import { ThemeProvider } from './components/ThemeContext'
+import ThemeSwitcher from './components/ThemeSwitcher'
+
+function App() {
+  return (
+    <>
+      <ThemeProvider>
+        <h1>Exemplo de Tema com useContext</h1>
+        <ThemeSwitcher/>
+      </ThemeProvider>  
+    </>
+  )
+}
+
+export default App
